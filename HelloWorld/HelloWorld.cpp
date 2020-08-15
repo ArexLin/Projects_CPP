782ee3c4c474e2ef722f3ba32796127994530151
Arex.Lin <arex.lin1980@gmail.com>
2020-08-15 17:42:41 +0800

    Sample code for Hello World with C++11 and VS Code

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <thread>
+
+using namespace std;
+
+#define MAX_LENGTH_A 10
+
+#define MAX_A 20
+#define MAX_LENGTH_ABC 30
+
+class TestClass
+{
+public:
+    void Hello_A();
+    void Hello_B();
+private:
+    int i;
+    int j;
+};
+
+void TestClass::Hello_A()
+{
+    int i = 10;
+    cout << endl;
+    cout << __FUNCTION__ << " >> " << i << endl;
+}
+void TestClass::Hello_B()
+{
+    int j = 20;
+    cout << __FUNCTION__ << " >> " << j << endl;
+}
+void Hello()
+{
+    cout << "Hello Thread" << endl;
+}
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    thread task(Hello);
+
+    for (const string &word : msg)
+    {
+        cout << word << " ";
+    }
+
+    if (1)
+    {
+        int j = 0;
+    }
+    else
+    {
+        Hello();
+    }
+
+    TestClass anInstance;
+    int i = ;
+
+    switch (i)
+    {
+    case 0:
+        anInstance.Hello_A();
+        break;
+
+    case 1:
+        anInstance.Hello_B();
+        break;
+
+    case 2:
+        break;
+
+    default:
+        break;
+    }
+
+    cout << endl;
+    task.join();
+}
